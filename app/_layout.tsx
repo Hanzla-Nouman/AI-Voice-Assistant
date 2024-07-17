f76232db3cb97bdbc35799ad46017d5f450cb9d7
Hanzla-Nouman <hanzlanouman514@gmail.com>
2024-07-17 15:39:19 +0500

    Done with welcome screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Index from './index';
+import Home from './Home';
 
 
 const Stack = createNativeStackNavigator();
@@ -14,6 +15,7 @@ function App() {
     <NavigationContainer independent={true}>
       <Stack.Navigator>
         <Stack.Screen name="Index" component={Index} options={{headerShown:false}} />
+        <Stack.Screen name="Home" component={Home} options={{headerShown:false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,43 @@
-import { Text, View } from "react-native";
-
+import { useNavigation } from "@react-navigation/native";
+import {
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 export default function Index() {
+  const navigation = useNavigation()
   return (
-  
-      <Text className="text-3xl font-bold bg-slate-800 text-yellow-400">Edit app/index.tsx to edit this screen.</Text>
-    
+    <>
+      <SafeAreaView className=" flex-1 my-7 flex items-center justify-around">
+        <View className="space-y-2">
+          <Text style={{ fontSize: hp(5) }} className=" text-center font-bold text-gray-700">
+            Uaghoos
+          </Text>
+          <Text style={{ fontSize: hp(2) }}  className=" text-center text-gray-600 tracking-wider font-semibold">I am here to help you with your queries.</Text>
+        </View>
+        <Image
+        style={{ height: hp(50),width: wp(75) }}
+          source={require("../assets/images/bots.png")}
+          className="w-72 h-72"
+        />
+        <TouchableOpacity 
+        onPress={()=>navigation.navigate('Home')}
+          style={{width: wp(80) }}
+        className="bg-emerald-700 p-4 mx-5 rounded-2xl">
+          <Text
+            style={{ fontSize: hp(3.5) }}
+            className="font-semibold text-white text-center"
+          >
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
   );
 }
