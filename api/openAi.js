2ce4358485221a74a2f4241bd7ff313c1bd82512
Hanzla-Nouman <hanzlanouman514@gmail.com>
2024-08-02 15:05:53 +0500

    Working on api

---
+++ api/openAi.js
@@ -0,0 +1,39 @@
+import { api_key } from "@/constants";
+import axios from "axios";
+
+
+const client = axios.create({
+    headers: {
+        "Authorization":"Bearer "+api_key,
+        "Content-Type":"application/json"
+     }
+  });
+
+
+const chatGptEndpoint = 'https://api.openai.com/v1/chat/completions';
+const dalleEndpoint = 'https://api.openai.com/v1/images/generations';
+
+export const apiCall = async(prompt,messages)=>{
+    try {
+
+        console.log("trying")
+        const res = await axios.post(chatGptEndpoint,{
+          
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "user",
+                content: `Does this message want to generate a new message an Ai picture,image,art or anything similiar? ${prompt}  .Simply answer with yes or no`
+              }]
+        },{  headers: {
+            "Authorization":"Bearer "+api_key,
+            "Content-Type":"application/json"
+         },})
+        console.log("Please enter")
+        console.log(res)
+
+    } catch (error) {
+        console.log("Here is error",error)
+        return
+    }
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,17 +6,29 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Index from './index';
 import Home from './Home';
+import { apiCall } from '@/api/openAi';
 
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await apiCall('What is Burj Khalifa');
+      } catch (error) {
+        console.error("API call error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (<>
   
-    <NavigationContainer independent={true}>
+    <NavigationContainer independent={true} >
       <Stack.Navigator>
         <Stack.Screen name="Index" component={Index} options={{headerShown:false}} />
-        <Stack.Screen name="Home" component={Home} options={{headerShown:false}} />
+        <Stack.Screen name="Home"  component={Home} options={{headerShown:false}} />
       </Stack.Navigator>
     </NavigationContainer>
     </>
